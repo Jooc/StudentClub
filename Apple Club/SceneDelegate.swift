50052cd9e9610e46e9e3d635d283f39c4a72ce78
齐旭晨 <1023578360@qq.com>
2020-04-24 10:59:27 +0800

    Revert "Gradation"
    This reverts commit ba820fae27e6aaeb182a4f28eb3d38a20c47785b.

--- Apple Club/SceneDelegate.swift
+++ Apple Club/SceneDelegate.swift
@@ -20,8 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let store = Store.Sample()
-        let contentView = MainTab().environmentObject(store)
+        let contentView = ContentView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
